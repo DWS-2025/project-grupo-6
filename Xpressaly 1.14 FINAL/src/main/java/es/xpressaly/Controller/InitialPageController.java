1bac11d2f7bb8897f4ee5ee2b2636ee9c5e5f66e
Laura-261 <146727498+Laura-261@users.noreply.github.com>
2025-03-14 12:07:26 +0100

    Update InitialPageController.java

--- Xpressaly 1.14 FINAL/src/main/java/es/xpressaly/Controller/InitialPageController.java
+++ Xpressaly 1.14 FINAL/src/main/java/es/xpressaly/Controller/InitialPageController.java
@@ -1,19 +1,12 @@
 package es.xpressaly.Controller;
-import org.springframework.beans.factory.annotation.Autowired;
+
 import org.springframework.stereotype.Controller;
 import org.springframework.web.bind.annotation.GetMapping;
-
-import es.xpressaly.Model.User;
-import es.xpressaly.Service.UserService;
-
 import org.springframework.ui.Model;
 
 
 @Controller
 public class InitialPageController {
-
-    @Autowired
-    private UserService userService;
 
     @GetMapping("/")
     public String welcome(Model model) {
